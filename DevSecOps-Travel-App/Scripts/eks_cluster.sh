f8c4dcd887edceb033cf928f48e92a031a4ef6b1
Uzair Khan <74897912+UXHERI@users.noreply.github.com>
2025-08-07 19:56:15 +0500

    Update eks_cluster.sh

--- DevSecOps-Travel-App/Scripts/eks_cluster.sh
+++ DevSecOps-Travel-App/Scripts/eks_cluster.sh
@@ -1,20 +1,54 @@
-eksctl create cluster --name=wanderlust \
-                    --region=us-east-1 \
-                    --version=1.30 \
-                    --without-nodegroup
+#!/bin/bash
 
-eksctl utils associate-iam-oidc-provider \
-  --region us-east-1 \
-  --cluster wanderlust \
-  --approve
+<< task
+Creating EKS Cluster, associating IAM OIDC provider, 
+and adding Node Group for Wanderlust Mega Project.
+task
 
-eksctl create nodegroup --cluster=wanderlust \
-                     --region=us-east-1 \
-                     --name=wanderlust \
-                     --node-type=t2.large \
-                     --nodes=2 \
-                     --nodes-min=2 \
-                     --nodes-max=2 \
-                     --node-volume-size=29 \
-                     --ssh-access \
-                     --ssh-public-key=eks-nodegroup-key 
+create_cluster() {
+  echo "Creating EKS cluster..."
+  eksctl create cluster --name="wanderlust" \
+                        --region="us-east-1" \
+                        --version="1.30" \
+                        --without-nodegroup
+}
+
+associate_oidc() {
+  echo "Associating IAM OIDC provider..."
+  eksctl utils associate-iam-oidc-provider \
+    --region "us-east-1" \
+    --cluster "wanderlust" \
+    --approve
+}
+
+create_nodegroup() {
+  echo "Creating node group..."
+  eksctl create nodegroup --cluster="wanderlust" \
+                          --region="us-east-1" \
+                          --name="wanderlust" \
+                          --node-type="t2.large" \
+                          --nodes=2 \
+                          --nodes-min=2 \
+                          --nodes-max=2 \
+                          --node-volume-size=29 \
+                          --ssh-access \
+                          --ssh-public-key="eks-nodegroup-key"
+}
+
+echo "********** EKS CLUSTER SETUP STARTED **********"
+if ! create_cluster; then
+  echo "EKS CLUSTER CREATION FAILED!!!"
+  exit 1
+fi
+
+if ! associate_oidc; then
+  echo "IAM OIDC ASSOCIATION FAILED!!!"
+  exit 1
+fi
+
+if ! create_nodegroup; then
+  echo "NODE GROUP CREATION FAILED!!!"
+  exit 1
+fi
+
+echo "********** EKS CLUSTER SETUP COMPLETED **********"
